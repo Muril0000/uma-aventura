fc8bd2bc7743b555dd7f931170c1d85edb6758d3
Muril0000 <00001084489065sp@al.educacao.sp.gov.br>
2024-09-12 14:35:50 -0300

    sla

---
+++ script.js
@@ -0,0 +1,13 @@
+const avance = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click' . function(){
+      const atual = documents.queryselector('.ativo');
+      const proximoPasso = 'passo-' + this.getAttribute('data-');
+
+atual.ClassList.remove('ativo');
+document.getElementById(proximoPasso).ClassList.add('ativo');
+
+      })
+    
+    })
